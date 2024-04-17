a42966a182c5f751b43da6c318c4d25224f13410
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-04-17 09:50:47 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+
+let answer = await inquirer.prompt([{
+    name: 'quest',
+    type: 'input',
+    message: 'Enter your sentence to count words?',
+}])
+
+const sentence = answer.quest.trim().split(" ")
+
+console.log(sentence);
+
+console.log(`Here is your sentence Length ${sentence.length}`);
+
+
+
+
+
